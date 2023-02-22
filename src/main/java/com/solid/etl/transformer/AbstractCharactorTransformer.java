3162c231eea74832d27e14caf61b48d2ca3edd5c
gowithai <dipakpatilcse@gmail.com>
2023-02-23 01:23:05 +0530

    LSP comment

--- src/main/java/com/solid/etl/transformer/AbstractCharactorTransformer.java
+++ src/main/java/com/solid/etl/transformer/AbstractCharactorTransformer.java
@@ -1,11 +1,13 @@
 package com.solid.etl.transformer;
 
+// To avoid Lickshov Principle Violation Added Class
 public abstract class AbstractCharactorTransformer implements DataTransformer{
 
     public boolean isNumeric(String data){
         return false;
     }
 
+    // WordCorrectorTransformer does not support, so here we created new class to avoid LSP violation
     abstract  boolean validateFinalWord(String data);
 
 }
